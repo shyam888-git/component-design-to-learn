c3d193b2cf3957426f54e20d2c08803f2c472c5d
unknown <ssk66221@gmail.com>
2024-06-06 23:21:57 +0545

    button-group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import ButtonComponents from "./components/ButtonComponent";
+import ButtonGroup from "./components/ButtonGroup";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <ButtonComponents />
+      {/* <ButtonComponents /> */}
+      <ButtonGroup/>
     </div>
   );
 }
