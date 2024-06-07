17e1dea8781ba49c9f02a2bad30410463d5e64a8
unknown <ssk66221@gmail.com>
2024-06-07 08:35:06 +0545

    card-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@ import Image from "next/image";
 import ButtonComponents from "./components/ButtonComponent";
 import ButtonGroup from "./components/ButtonGroup";
 import ButtonGroupLinks from "./components/ButtonGroupLinks";
+import ButtonGroupLink2 from "./components/ButtonGroupLink2";
+import Example1 from "./components/Card/Example1";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       {/* <ButtonComponents /> */}
       {/* <ButtonGroup/> */}
-      <ButtonGroupLinks />
+      {/* <ButtonGroupLink2/> */}
+      {/* <ButtonGroupLinks /> */}
+
+      <Example1/>
     </div>
   );
 }
